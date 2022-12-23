60445bd562a1e20ba56892b418490105c56d5b09
Moses Bakst <mosesbakst@gmail.com>
2022-12-23 15:20:25 -0500

    Update newyb.js

--- yearbook/newyb.js
+++ yearbook/newyb.js
@@ -1,87 +1,75 @@
-const mapSPT = {
-    'ק': 'q',
-    'ו': 'w',
-    'ר': 'r',
-    'ת': 't',
-    'י': 'y',
-    'פ': 'p',
-    'ס': 's',
-    'ד': 'd',
-    'ג': 'g',
-    'ה': 'h',
-    'כ': 'k',
-    'ל': 'l',
-    'ז': 'z',
-    'ח': 'x',
-    'צ': 'c',
-    'ב': 'b',
-    'נ': 'n',
-    'מ': 'm',
-    'ף': 'P',
-    'ך': 'K',
-    'ץ': 'C',
-    'ן': 'N',
-    'ש': '#',
-    'ם': 'M',
-    'ע': '(',
-    'א': ')',
-    'ט': '+',
-    ' ': ' ',
-    '.': '*',
-    ',': 'D'
-  };
-
-const mapDB =  {
-    'ס': 'q',
-    'ק': 'w',
-    'ר': 'e',
-    'ע': 'r',
-    'פ': 't',
-    'ש': 'y',
-    'ץ': 'u',
-    'י': 'i',
-    'ן': 'o',
-    'נ': 'p',
-    'ב': 'a',
-    'ף': 's',
-    'ה': 'd',
-    'ז': 'f',
-    'ח': 'g',
-    'ט': 'h',
-    'ת': 'j',
-    'כ': 'k',
-    'ם': 'l',
-    'מ': ';',
-    'ך': "'",
-    'ד': 'z',
-    'ג': 'x',
-    'ז': 'c',
-    'ח': 'v',
-    'ט': 'b',
-    'י': 'n',
-    'ך': 'm',
-    ' ': ' ',
-    '.': '*',
-    ',': 'D'
-  };
-
 var SD = prompt("1 for SPTiberian, 2 for Davka Bodel.");
 var hebrew = prompt("Input Hebrew text.");
+var english;
+var map;
+const mapSPT = {
+  ק: "q",
+  ו: "w",
+  ר: "r",
+  ת: "t",
+  י: "y",
+  פ: "p",
+  ס: "s",
+  ד: "d",
+  ג: "g",
+  ה: "h",
+  כ: "k",
+  ל: "l",
+  ז: "z",
+  ח: "x",
+  צ: "c",
+  ב: "b",
+  נ: "n",
+  מ: "m",
+  ף: "P",
+  ך: "K",
+  ץ: "C",
+  ן: "N",
+  ש: "#",
+  ם: "M",
+  ע: "(",
+  א: ")",
+  ט: "+",
+  " ": " ",
+  ".": "*",
+  ",": "D",
+};
 
-function hebrewToEnglish(mapping, hebrew) {
-  let englishWord = "";
+const DBmap = {
+  ס: "q",
+  ק: "w",
+  ר: "e",
+  ע: "r",
+  פ: "t",
+  ש: "y",
+  ץ: "u",
+  י: "i",
+  ן: "o",
+  נ: "p",
+  ב: "a",
+  ף: "s",
+  ה: "d",
+  ז: "f",
+  ח: "g",
+  ט: "h",
+  ך: "j",
+  כ: "k",
+  ל: "l",
+  ת: "z",
+  ר: "x",
+  ד: "c",
+  צ: "v",
+  ג: "b",
+  מ: "n",
+  ם: "m",
+  " ": " ",
+  ".": "*",
+  ",": "\\",
+};
 
-  for (var i = 0; i < hebrew.length; i++) {
-    englishWord += mapping[hebrew[i]] || hebrew[i];
-  }
+(SD===1) ? map = mapSPT: map = DBmap;
 
-  return englishWord;
+for (var i = 0; i < hebrew.length; i++) {
+    english += map[hebrew[i]] || hebrew[i];
 }
-
-if (SD == 1) {
-  var english = hebrewToEnglish(mapSPT, hebrew);
-} else {
-  var english = hebrewToEnglish(mapDB, hebrew);
-}
-
-englishArray.push(englishWord);
+english.reverse();
